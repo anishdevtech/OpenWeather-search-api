1b71a7cac78d960e54d1b6215143904afe49ec63
Anish Sharma <118909523+anishdevtech@users.noreply.github.com>
2023-12-03 12:24:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
 // Read city data from the JSON file
 const cityDataPath = path.join(__dirname, 'city.list.min.json');
 const cityData = JSON.parse(fs.readFileSync(cityDataPath, 'utf8'));
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+
+// OpenWeatherMap API key (replace with your own key)
+const apiKey = process.env.api
+
 // API endpoint to get city information by name
 app.get('/city/:cityName', (req, res) => {
   const cityName = req.params.cityName.toLowerCase();
@@ -20,6 +22,34 @@ app.get('/city/:cityName', (req, res) => {
     res.json(matchingCity);
   } else {
     res.status(404).json({ error: 'City not found' });
+  }
+});
+
+// API endpoint to get city information by coordinates
+app.get('/getCityId', async (req, res) => {
+  const { lon, lat } = req.query;
+
+  try {
+    // Use OpenWeatherMap Geocoding API to get city name from coordinates
+    const geoApiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
+    const geoResponse = await axios.get(geoApiUrl);
+
+    const cityName = geoResponse.data[0]?.name;
+
+    if (cityName) {
+      // Search for the city in the local JSON data
+      const matchingCity = cityData.find(city => city.name.toLowerCase() === cityName.toLowerCase());
+
+      if (matchingCity) {
+        res.json(matchingCity);
+      } else {
+        res.status(404).json({ error: 'City not found in local data' });
+      }
+    } else {
+      res.status(404).json({ error: 'City not found in OpenWeatherMap data' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
